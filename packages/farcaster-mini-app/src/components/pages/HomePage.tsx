420b13cd7bd61d3c181e5fccfe3a339eb5ca3f06
thisisashugupta <ashugpt1812@gmail.com>
2025-06-14 00:28:02 +0530

    feat: show Your Pots only when wallet is connected

--- packages/farcaster-mini-app/src/components/pages/HomePage.tsx
+++ packages/farcaster-mini-app/src/components/pages/HomePage.tsx
@@ -3,11 +3,13 @@
 import YourPots from '../sections/YourPots';
 import PotList from '../sections/PotList';
 import Hero from '../sections/Hero';
+import { useAccount } from 'wagmi';
 
 export default function HomePage() {
+  const { isConnected, address } = useAccount();
   return (
     <div>
-      <YourPots />
+      {isConnected && !!address ? <YourPots /> : null}
       <Hero />
       <PotList />
     </div>
