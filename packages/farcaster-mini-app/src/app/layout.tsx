ab28904faf3377ab60cf4a61276d085594f4dcc9
thisisashugupta <ashugpt1812@gmail.com>
2025-06-14 09:47:13 +0530

    fix: font

--- packages/farcaster-mini-app/src/app/layout.tsx
+++ packages/farcaster-mini-app/src/app/layout.tsx
@@ -13,14 +13,58 @@ export const metadata: Metadata = {
 const satoshi = localFont({
   src: [
     {
-      path: '../fonts/Satoshi-Variable.ttf',
+      path: '../fonts/Satoshi-Light.woff2',
+      weight: '300',
       style: 'normal',
     },
     {
-      path: '../fonts/Satoshi-VariableItalic.ttf',
+      path: '../fonts/Satoshi-LightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Satoshi-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Satoshi-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Satoshi-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Satoshi-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Satoshi-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Satoshi-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Satoshi-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Satoshi-BlackItalic.woff2',
+      weight: '900',
       style: 'italic',
     },
   ],
+  display: 'swap',
+  variable: '--font-satoshi',
 });
 
 export default async function RootLayout({
@@ -30,7 +74,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang='en' className={satoshi.className}>
-      <body className='antialiased'>
+      <body className='antialiased font-sans'>
         <Providers>{children}</Providers>
       </body>
     </html>
