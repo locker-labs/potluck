1ffa1b44c15ec4d48c771451a366c514c559f3cf
thisisashugupta <ashugpt1812@gmail.com>
2025-07-27 02:43:42 +0530

    fix: add hyperlink to logo in navbar

--- packages/farcaster-mini-app/src/components/NavBar.tsx
+++ packages/farcaster-mini-app/src/components/NavBar.tsx
@@ -7,6 +7,7 @@ export default function NavBar() {
     <nav className='backdrop-blur fixed top-0 left-0 right-0 z-50 shadow-sm px-4 pt-5 pb-3'>
       <div className='w-full flex justify-between items-center'>
         <div className='flex items-center space-x-2'>
+          <Link href='/'>
           <Image
             src='/logo.png'
             alt='Logo'
@@ -15,6 +16,7 @@ export default function NavBar() {
             className='min-w-[40px] max-w-[40px] aspect-auto object-cover'
             draggable='false'
           />
+          </Link>
           <Link href='/' className='text-[24px] font-bold uppercase'>
             {APP_NAME}
           </Link>
