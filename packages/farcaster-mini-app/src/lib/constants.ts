856e889fc9fbf2376ba9f6c3ae7bd829cca34a3a
thisisashugupta <ashugpt1812@gmail.com>
2025-07-30 17:37:48 +0530

    fix: allowed origins parsing

--- packages/farcaster-mini-app/src/lib/constants.ts
+++ packages/farcaster-mini-app/src/lib/constants.ts
@@ -42,4 +42,6 @@ export const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL;
 
 export const NEYNAR_WEBHOOK_URL = env.NEYNAR_WEBHOOK_URL;
 
-export const ALLOWED_ORIGINS = env.ALLOWED_ORIGINS.split(',').map((origin) => origin.trim());
+export const ALLOWED_ORIGINS = env.ALLOWED_ORIGINS
+  ? env.ALLOWED_ORIGINS.split(',').map((origin) => origin.trim())
+  : [];
