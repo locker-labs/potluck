42b715e4ff20f8bb521b43534dc2b77fcb68523a
thisisashugupta <ashugpt1812@gmail.com>
2025-08-10 02:01:33 +0530

    feat: add profile icon in navbar

--- packages/farcaster-mini-app/src/components/NavBar.tsx
+++ packages/farcaster-mini-app/src/components/NavBar.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { APP_NAME } from '@/lib/constants';
+import { useFrame } from '@/providers/FrameProvider';
 
 export default function NavBar() {
+  const { context } = useFrame();
+  const pfpUrl = context?.user.pfpUrl ?? '/pfp_100px.webp';
+
   return (
     <nav className='backdrop-blur fixed top-0 left-0 right-0 z-50 shadow-sm px-4 pt-5 pb-3'>
       <div className='w-full flex justify-between items-center'>
@@ -20,6 +24,18 @@ export default function NavBar() {
           <Link href='/' className='text-[24px] font-bold uppercase'>
             {APP_NAME}
           </Link>
+        </div>
+
+        <div className='flex items-center'>
+          <Image
+            src={pfpUrl}
+            alt='Profile'
+            width={40}
+            height={40}
+            className='rounded-full object-cover transition-all duration-100 ease-in outline outline-2 hover:outline-4 outline-app-cyan'
+            draggable='false'
+          />
+          
         </div>
       </div>
     </nav>
