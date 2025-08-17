2c32f6fcfe2ecfcee76d3e1c6437e3eb89871a23
thisisashugupta <ashugpt1812@gmail.com>
2025-08-17 08:20:08 +0530

    refactor: use links.ts for mini app links

--- packages/farcaster-mini-app/src/lib/helpers/links.ts
+++ packages/farcaster-mini-app/src/lib/helpers/links.ts
@@ -1,5 +1,9 @@
 import { MINI_APP_URL } from '@/lib/constants';
 
+export function getInviteLink(potId: bigint): string {
+  return `${MINI_APP_URL}/pot/${potId}`;
+}
+
 export const getProfileLink = (address: string) => {
   return `${MINI_APP_URL}/profile/${address}`;
 };
